240c7c6b5b63ff1660bf651fdb07928d62c690cd
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-09-24 14:56:10 +0200

    Q1.3

--- TD2/src/Hanoi.java
+++ TD2/src/Hanoi.java
@@ -14,11 +14,26 @@ public class Hanoi {
     }
     public void resoudre(int n){
         resoudreAux(n,1,3);
+        System.out.println(calcul(n,1,3));
+    }
+
+    public int calcul(int n, int i, int j){
+        if(n==1){
+           return 1;
+        }
+        else{
+            int k=6-i-j;
+            int a =calcul(n-1,i,k);
+            int b = calcul(n-1,k,j);
+            return a+b+1;
+
+        } //f(n)=2^n-1
     }
 
     public static void main(String[] args){
         Hanoi h1= new Hanoi();
         //h1.resoudreAux(4,2,1);
         h1.resoudre(3);
+
     }
 }
