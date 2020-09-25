119e2c964412efafa62aeb6d1cc41512ad5866df
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-09-25 11:26:28 +0200

    Baguenaudier 1

--- TD2/src/Baguenaudier.java
+++ TD2/src/Baguenaudier.java
@@ -19,7 +19,7 @@ public class Baguenaudier {
     }
 
     public void remplir(int n) {
-        if (n == 1) {
+        if (n == 0) {
             tab[0] = true;
         } else {
             remplir(n - 1);
@@ -27,12 +27,45 @@ public class Baguenaudier {
         afficher();
     }
 
+    public void  remplir2(int n) {
+        if (n == 1) {
+            System.out.println(0);
+        } else if(n > 1) {
+            //-----
+            remplir2(n - 1);
+            //****-
+            vider2(n-2);
+            //---*-
+            //ici cache jouable 0 et n-1
+            System.out.println(n-1);
+            //---**
+            remplir2(n - 2);
+            //*****
+        }
+    }
+
+    public void vider2(int n) {
+        if (n == 1) {
+            System.out.println(0);
+        }
+        else if(n>1) {
+            //*****
+            vider2(n - 1);
+            //---**
+            System.out.println(n -1);
+            //---*-
+            remplir2(n - 2);
+            //****-
+            vider2(n-2);
+            //*****
+        }
+    }
 
 
     public static void main (String[]args){
             Baguenaudier b1 = new Baguenaudier(4);
             b1.afficher();
-            b1.remplir(3);
+            b1.remplir2(3);
 
 
     }
