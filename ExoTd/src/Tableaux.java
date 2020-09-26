464c12c9f5b9ffb858cd28bfebd19eeefc7e2d5e
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-09-27 00:44:30 +0200

    Test palindrome marche pas

---
+++ ExoTd/src/Tableaux.java
@@ -0,0 +1,48 @@
+
+public class Tableaux {
+    public static boolean palindromeAux(Character []t, int i,int j){
+        /*if(i>=j){
+            return true;
+        }*/
+        if(i==j){
+            return true;
+        }
+        if(i==j-1){
+            return t[i]==t[j];
+        }
+        if(t[i]!=t[j]){
+            return false;
+        }
+        else{
+            return palindromeAux(t,i+1,j-1);
+        }
+    }
+
+    public static Character[] toCharacterArray( String s ) {
+
+        if ( s == null ) {
+            return null;
+        }
+
+        int len = s.length();
+        Character[] array = new Character[len];
+        for (int i = 0; i < len ; i++) {
+            array[i] = new Character(s.charAt(i));
+        }
+
+        return array;
+    }
+
+    public static boolean palindrome(Character []t){
+        return palindromeAux(t,0,t.length-1);
+    }
+
+    public static void main(String[] args){
+        String str = "ii";
+        Character[] charArray = toCharacterArray(str);
+
+        System.out.println(palindrome(charArray));
+
+
+    }
+}
