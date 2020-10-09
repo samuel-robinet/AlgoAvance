65eab896d2c41c1bebee4a23aab04070ce779ddc
Samuel Robinet <Samuel.robinet@etu.umontpellier.fr>
2020-10-09 11:14:21 +0200

    EX 5

--- TD3/src/Arbre.java
+++ TD3/src/Arbre.java
@@ -49,10 +49,44 @@ public class Arbre {
 
     }
 
+    //marche pour tout l'arbre
+    public boolean pereFilsEgaux2(){
+        if (estVide()){
+            return false;
+        }
+        /*
+        if(!filsD.estVide()){
+            if(val==filsD.val){
+                return true;
+            }
+        }
+        if(!filsG.estVide()){
+            if(val==filsG.val){
+                return true;
+            }
+        }*/
+        if((!filsD.estVide() && val==filsD.val)||(!filsG.estVide() && val==filsG.val)){
+            return true;
+        }
+        return filsG.pereFilsEgaux2() || filsD.pereFilsEgaux2();
+    }
+    //doit marcher pour de x consecutif
+    public boolean pereFilsEgaux(){
+        if (estVide()){
+            return false;
+        }
+        if((!filsD.estVide() && val==filsD.val)||(!filsG.estVide() && val==filsG.val)){
+            return true;
+        }
+        return filsG.pereFilsEgaux() && filsD.pereFilsEgaux();
+    }
+
     public static void main(String[] args){
         Arbre A1 = new Arbre(1,new Arbre(2),new Arbre(3));
 
         Arbre A2 = new Arbre(1,new Arbre(2,new Arbre(4), new Arbre(5)),new Arbre(3,new Arbre(6),new Arbre(7)));
+
+
         System.out.println(A1.chercheNoeudInterne(1));
 
         System.out.println(A1.chercheFeuille(3));
@@ -60,6 +94,12 @@ public class Arbre {
         System.out.println(A2.chercheNoeudInterne(0));
 
         System.out.println(A2.chercheFeuille(1));
+
+        Arbre A31=new Arbre(2,new Arbre(4), new Arbre(5));
+        Arbre A32=new Arbre(3,new Arbre(6),new Arbre(7));
+        Arbre A3 = new Arbre(1,A31,A32);
+
+        System.out.println(A3.pereFilsEgaux());
 
     }
 }
